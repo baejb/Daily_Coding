b492270411d1970e5c1d34c3570ee205d6e62bcd
baejb <bjb6478@naver.com>
2024-03-07 23:28:35 +0900

    ATM_240307

--- baekjoon/11399(ATM).js
+++ baekjoon/11399(ATM).js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'example.txt';
 const input = fs.readFileSync(filePath).toString().split('\n');
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+// 걸리는 시간이 작은 순으로 정렬 
+arr.sort((a,b)=> a-b);
+
+let sum = 0;
+let answer = [];
+arr.forEach((x)=>{
+    sum+= x ;
+    answer.push(sum);
+})
+// 앞 시간과 더한 시간을 저장한 배열 answer의 원소 값을 모두 저장한 값이 정답 
+console.log(answer.reduce((acc, cur)=> acc+cur));
